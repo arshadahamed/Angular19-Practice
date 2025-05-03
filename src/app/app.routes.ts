3339ededa349835a164273d365b8cee87436191f
arshadahamed <arshadpayoneer@gmail.com>
2025-05-03 18:31:42 +0530

    WildCard route Created for 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ServicesComponent } from './services/services.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+  {path: '', component: AppComponent}, // Default route
   {path: 'home', component: HomeComponent},
   {path: 'about', component:AboutComponent},
   {path: 'contact', component:ContactComponent},
   {path: 'services', component:ServicesComponent},
-  {path:'employee/:empName', component:EmployeeComponent},
+  {path:'employee/:empName', component:EmployeeComponent}, // Route with a parameter Dynamic Routes
+  {path:'**', component:PageNotFoundComponent} // Wildcard route for a 404 page
 ];
